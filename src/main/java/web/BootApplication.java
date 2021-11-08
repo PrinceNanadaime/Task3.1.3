0229761e2efd9bdc17f32d70b6103ef3c60b0863
Nikita <kotenkov.nikita@mail.ru>
2021-11-08 22:58:07 +0300

    First commit

--- src/main/java/web/BootApplication.java
+++ src/main/java/web/BootApplication.java
@@ -2,9 +2,16 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
